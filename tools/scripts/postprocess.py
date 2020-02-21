66abcd7e02b39294c9999f477049230109a02392
Þórunn Arnardóttir <50205919+thorunna@users.noreply.github.com>
2020-02-21 13:19:45 +0000

    Update postprocess.py

--- tools/scripts/postprocess.py
+++ tools/scripts/postprocess.py
@@ -3,10 +3,10 @@ import sys
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
 
-with open(inputFile) as f:
+with open(inputFile, encoding='utf-8') as f:
     text = f.read()
 
 text = text.replace('*','-')
 
-with open(outputFile, 'w') as f:
+with open(outputFile, 'w', encoding='utf-8') as f:
     f.write(text)
