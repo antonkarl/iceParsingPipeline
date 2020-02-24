c100b388ee39557033053fc294875bc4281d96ee
Þórunn Arnardóttir <50205919+thorunna@users.noreply.github.com>
2020-02-24 14:30:53 +0000

    Update preprocess.py

--- tools/scripts/preprocess.py
+++ tools/scripts/preprocess.py
@@ -4,7 +4,7 @@ import re
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
 
-with open(inputFile) as f:
+with open(inputFile, encoding='utf-8') as f:
     text = f.read()
 
 # Let there be whitespace around punctuation symbols
@@ -18,7 +18,7 @@ text = text.replace('\"',' \" ')
 
 text = re.sub(' +',' ',text)
 
-with open(outputFile, 'w') as f:
+with open(outputFile, 'w', encoding='utf-8') as f:
     f.write(text)
     
 
